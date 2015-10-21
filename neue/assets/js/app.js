58563e0612b12d4a2535804b018525c7ab1e5a50
Adrian Gyuricska <a.gyuricska@gmail.com>
2015-10-20 21:54:11 -0600

    Changes

--- neue/assets/js/app.js
+++ neue/assets/js/app.js
@@ -5,90 +5,235 @@
  * @author Emigrant Partners, 2015
  */
 (function() {
-  /**
-   * Setup of the map, powered by Mapbox/Leaflet.
-   */
-  var Mapbox = L.mapbox;
+  function scrolling() {
+    var lastScrollTop;
+    var st = $(window).scrollTop();
 
-  Mapbox.accessToken = 'pk.eyJ1Ijoicm9ja2V0cyIsImEiOiIyM2I2NWQ4ZDE2Y2RkMWRlYTgwNWY3NjhlNWEwODYxZCJ9.JFOQm4P8BS70ymTjazN6Zg';
-  window.map = Mapbox.map('map', 'mapbox.streets', { zoomControl: false });
+    $(".header").each(function(){
+      var st = $(this).scrollTop();
+      if (st > lastScrollTop){
+        $('.header').removeClass("is-scrolled--up").addClass("is-scrolled--down");
+      } else {
+        $('.header').removeClass("is-scrolled--down").addClass("is-scrolled--up");
+      }
+      lastScrollTop = st;
 
-  window.map.setView(['53.5409800211682', '-113.50206792354584'], 19);
+      if ($(window).scrollTop() > 50) {
+        $('.header').addClass("is-scrolled");
+      } else {
+        $('.header').removeClass("is-scrolled");
+      }
+    });
+  }
 
-  window.map.dragging.disable();
-  window.map.scrollWheelZoom.disable();
-  window.map.touchZoom.disable();
-  // window.map.tap.disable();
+  scrolling();
+  window.addEventListener('scroll', scrolling);
 
-  window.displayedMarkers = [];
+  var map;
+  function initMap() {
+    var styles = [
+      {
+          "featureType": "administrative.locality",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#ff9500"
+              },
+              {
+                  "saturation": 7
+              },
+              {
+                  "lightness": 19
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.locality",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#ff0000"
+              },
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#ffffff"
+              },
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 100
+              },
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ffa700"
+              },
+              {
+                  "saturation": -52
+              },
+              {
+                  "lightness": -10
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "hue": "#ff9500"
+              },
+              {
+                  "saturation": -93
+              },
+              {
+                  "lightness": 31
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#333132"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "color": "#fbf7f3"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "hue": "#c79c60"
+              },
+              {
+                  "saturation": -93
+              },
+              {
+                  "lightness": -2
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#c79c60"
+              },
+              {
+                  "saturation": -52
+              },
+              {
+                  "lightness": -10
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#ffa700"
+              },
+              {
+                  "saturation": 10
+              },
+              {
+                  "lightness": 69
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "hue": "#c79c60"
+              },
+              {
+                  "saturation": -78
+              },
+              {
+                  "lightness": 67
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      }
+  ]
 
-  var highlights = {
-    'lifestyle': {
-      waypoints: [
-        ['53.541437', '-113.500654'],
-        ['53.541361', '-113.502104'],
-        ['53.540918', '-113.501282'],
-        ['53.541361', '-113.502134'],
-        ['53.540760', '-113.498693'],
-        ['53.541789', '-113.499441']
-      ],
-      center: ['53.54113941051847', '-113.5004585981369'],
-      zoom: 18
-    },
-    'residential': {
-      waypoints: [
-        ['53.5430154', '-113.5032441'],
-        ['53.5407836', '-113.5079787'],
-        ['53.543225', '-113.499664'],
-        ['53.5417052', '-113.5064083']
-      ],
-      center: ['53.54196503774724', '-113.50528120994566'],
-      zoom: 17
-    },
-    'transit': {
-      waypoints: [
-        ['53.5409823', '-113.5061451'],
-        ['53.5410606', '-113.5010208'],
-        ['53.5410825', '-113.4982893'],
-        ['53.5410825', '-113.4982893'],
-        ['53.5409374', '-113.4991976'],
-        ['53.5409374', '-113.4991976'],
-        ['53.5403939', '-113.5048541']
-      ],
-      center: ['53.5409800211682', '-113.50206792354584'],
-      zoom: 17
-    }
-  };
-
-  /**
-   * Setup of the click targets
-   */
-  $(".map__feature").on("click", function(e) {
-    e.preventDefault();
-    var link = $(e.target);
-
-    // remove current markers from map
-    for (var i = 0; i < window.displayedMarkers.length; i++) {
-      window.map.removeLayer(window.displayedMarkers[i]);
-    }
-
-    $('.map__feature--active').removeClass('map__feature--active');
-    link.addClass('map__feature--active');
-
-    var highlight = link.data('mapHighlight');
-    var feature = highlights[highlight];
-
-    var waypoints = feature.waypoints;
-    var center = feature.center;
-    var zoom = feature.zoom;
-    window.map.setView(center, zoom);
-
-    for (i = 0; i < waypoints.length; i++) {
-      var marker = L.marker(waypoints[i]);
-      window.displayedMarkers.push(marker);
-      marker.addTo(window.map);
-    }
-  });
-
-  $('.map__feature')[1].click();
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 53.5409305, lng: -113.5019699},
+      scrollwheel: false,
+      zoom: 15,
+      styles: styles
+    });
+  }
+  window.onload = function() {
+    initMap();
+  }
 })();
